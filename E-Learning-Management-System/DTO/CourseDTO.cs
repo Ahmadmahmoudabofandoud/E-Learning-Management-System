964dbd2e6a1e3d3ad7d8f0fc6c02f2736964493b
unknown <ahmedmahmoudabofandoud>
2024-09-27 20:17:22 +0300

    first commit

---
+++ E-Learning-Management-System/DTO/CourseDTO.cs
@@ -0,0 +1,11 @@
+﻿using E_Learning_Management_System.Models;
+
+namespace E_Learning_Management_System.DTO
+{
+    public class CourseDTO
+    {
+        public String Name { get; set; }
+        public string ImgPath { get; set; }
+        public int DurationInHours { get; set; }
+    }
+}
---
+++ E-Learning-Management-System/Models/Adminstrator.cs
@@ -0,0 +1,19 @@
+﻿using E_Learning_Management_System.Repository;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_Learning_Management_System.Models
+{
+    public class Adminstrator : IDeletable
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; } = false;
+
+        [ForeignKey("Account")]
+        public string? AccountId { get; set; }
+        public ApplicationUser? Account { get; set; }
+        [ForeignKey("Content")]
+        public int? ContentId { get; set; }
+        public Content? Content { get; set; }
+    }
+}
